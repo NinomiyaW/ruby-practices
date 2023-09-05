a3ea63700893df3307d7f069f3e6aa121077d0bf
Ninomiya Hikaru <h.ninomiya@i3-systems.com>
2023-09-05 10:52:49 +0900

    ご指摘いただいた箇所を修正いたしました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -2,19 +2,15 @@
 
 require 'debug'
 
-class Array
-  def strike?
-    return true if self[0] == 10
+# class Array
+#   def strike?
+#     self[0] == 10
+#   end
 
-    false
-  end
-
-  def spare?
-    return true if sum == 10
-
-    false
-  end
-end
+#   def spare?
+#     sum == 10
+#   end
+# end
 
 score = ARGV[0]
 scores = score.split(',')
@@ -30,32 +26,27 @@ scores.each do |s|
   end
 end
 # フレームごとに点を分割する
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
-p frames
+frames = shots.each_slice(2).to_a
 
 # 点数を計算する
 point = 0
 frames.each_with_index do |frame, count|
-
+  # binding.break
   if count >= 9
     point += frame.sum
     next
   end
 
   point +=
-    if frame.strike?
-      if frames[count + 1].strike?
-        30 if frames[count + 2].strike?
+    if frame[0] == 10
+      if frames[count + 1][0] == 10
         (20 + frames[count + 2][0])
       else
-        10 + frames[count + 1].sum
+        (10 + frames[count + 1].sum)
       end
 
-    elsif frame.spare?
-      10 + frames[count + 1][0]
+    elsif frame.sum == 10
+      (10 + frames[count + 1][0])
     else
       frame.sum
     end
