a820449552dbc42968ee3d2e4df482a6209802e4
Ninomiya Hikaru <h.ninomiya@i3-systems.com>
2023-09-06 10:25:27 +0900

    ご指摘いただいた箇所を修正いたしました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,25 +1,13 @@
 # frozen_string_literal: true
 
-require 'debug'
-
-# class Array
-#   def strike?
-#     self[0] == 10
-#   end
-
-#   def spare?
-#     sum == 10
-#   end
-# end
-
 score = ARGV[0]
 scores = score.split(',')
 shots = []
-
+MAX_POINT = 10
 # オプションから入力したスコアを計算できる形にする
 scores.each do |s|
   if %w[x X].include?(s)
-    shots << 10
+    shots << MAX_POINT
     shots << 0
   else
     shots << s.to_i
@@ -31,24 +19,19 @@ frames = shots.each_slice(2).to_a
 # 点数を計算する
 point = 0
 frames.each_with_index do |frame, count|
-  # binding.break
-  if count >= 9
-    point += frame.sum
-    next
-  end
+  point += frame.sum
+  next if count >= 9 || frame.sum != 10
 
+  # ストライクまたはスペアの場合の計算
   point +=
-    if frame[0] == 10
-      if frames[count + 1][0] == 10
-        (20 + frames[count + 2][0])
+    if frame[0] == MAX_POINT
+      if frames[count + 1][0] == MAX_POINT
+        (MAX_POINT + frames[count + 2][0])
       else
-        (10 + frames[count + 1].sum)
+        frames[count + 1].sum
       end
-
-    elsif frame.sum == 10
-      (10 + frames[count + 1][0])
     else
-      frame.sum
+      (frames[count + 1][0])
     end
 end
 
