b679ae6acdea50d1076e20ad8ecd09728a9368ad
Ninomiya Hikaru <h.ninomiya@i3-systems.com>
2023-09-07 10:11:16 +0900

    ご指摘いただいた内容を修正致しました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -20,18 +20,18 @@ frames = shots.each_slice(2).to_a
 point = 0
 frames.each_with_index do |frame, count|
   point += frame.sum
-  next if count >= 9 || frame.sum != 10
+  next if count >= 9 || frame.sum != MAX_POINT
 
   # ストライクまたはスペアの場合の計算
   point +=
     if frame[0] == MAX_POINT
       if frames[count + 1][0] == MAX_POINT
-        (MAX_POINT + frames[count + 2][0])
+        MAX_POINT + frames[count + 2][0]
       else
         frames[count + 1].sum
       end
     else
-      (frames[count + 1][0])
+      frames[count + 1][0]
     end
 end
 
