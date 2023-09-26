0a84f5f3b5798f07e8f435e8bb0bcb39a16b5657
Ninomiya Hikaru <h.ninomiya@i3-systems.com>
2023-09-26 10:24:14 +0900

    ルートディレクトリ以外でもエントリ種別の判定ができるように修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -12,25 +12,25 @@ longest_name_length = hidden_removed_entries.max_by(&:length).length + 3
 # Array#transposeで要素数が3の配列を作るため、列(定数)を使ってまず行ごとにエントリを分割する
 row = (hidden_removed_entries.length.to_f / COLUMN).ceil
 
-# 行が列の数以下の場合は素直に縦方向に出力する
-if row < COLUMN
-  col_aligned_entries = hidden_removed_entries.each_slice(1).to_a
-else
-  row_aligned_entries = hidden_removed_entries.each_slice(row).to_a
-  # Array#transposeメソッドを使うには各要素の要素数を統一する必要がある
-  # 参照：https://patorash.hatenablog.com/entry/2021/03/17/163504
-  row_aligned_entries.map! { |entries_col| entries_col.values_at(0...row) }
-  # 縦に出力するため、行と列を入れ替える
-  col_aligned_entries = row_aligned_entries.transpose
-end
+col_aligned_entries =
+  # 行数が列数以下の場合、1個ずつ分割する(13行目で切り上げた値がファイルの項目数を超えるため)
+  if row < COLUMN
+    hidden_removed_entries.each_slice(1).to_a
+  else
+    row_aligned_entries = hidden_removed_entries.each_slice(row).to_a
+    # Array#transposeメソッドを使うには各要素の要素数を統一する必要がある
+    # 参照：https://patorash.hatenablog.com/entry/2021/03/17/163504
+    row_aligned_entries.map! { |entries_col| entries_col.values_at(0...row) }.transpose
+  end
 
 # 出力
 col_aligned_entries.each do |entries_col|
+  entry_path = curdir_fullpath == '/' ? curdir_fullpath : "#{curdir_fullpath}/"
   entries_col.each do |entry|
     # values_atで作り出したnil?は無視
     next if entry.nil?
 
-    entry_path = curdir_fullpath + entry
+    entry_path += entry
     if File.symlink?(entry_path)
       print "#{entry}@".ljust(longest_name_length)
     elsif File.directory?(entry_path)
