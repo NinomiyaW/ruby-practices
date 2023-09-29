33c6ac6d92c39fa66a3e3313465ec9e9d75f0c36
Ninomiya Hikaru <h.ninomiya@i3-systems.com>
2023-09-29 11:50:56 +0900

    ご指摘いただいた箇所について修正をいたしました

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,48 +1,49 @@
 # frozen_string_literal: true
 
 COLUMN = 3
-SPACE = 3
-
+SPACE = 1
 def main
-  curdir_fullpath = "#{Dir.getwd}/"
-  curdir_entries = Dir.entries(curdir_fullpath).sort
-  hidden_removed_entries = curdir_entries.delete_if { |entry| entry.start_with?('.') }
-  row = (hidden_removed_entries.length.to_f / COLUMN).ceil
-  aligned_entries = get_aligned_entries(row, hidden_removed_entries)
+  current_path = "#{Dir.getwd}/"
+  current_entries = Dir.entries(current_path).sort
+  hidden_removed_entries = current_entries.delete_if { |entry| entry.start_with?('.') }
+  distincted_entries = get_distincted_entries(hidden_removed_entries, current_path)
+  row = (distincted_entries.length.to_f / COLUMN).ceil
+  aligned_entries = get_aligned_entries(row, distincted_entries)
 
-  col_width = get_space(curdir_entries)
-  aligned_entries.each do |entries_col|
-    col_print(entries_col, curdir_fullpath, col_width)
+  col_width = get_width(distincted_entries)
+  aligned_entries.each do |col_entries|
+    print_col(col_entries, col_width)
     puts
   end
 end
 
-def get_space(entries)
+def get_distincted_entries(entries, absolute_path)
+  entries.map do |entry|
+    entry_path = absolute_path + entry
+    if File.symlink?(entry_path)
+      "#{entry}@"
+    elsif File.directory?(entry_path)
+      "#{entry}/"
+    else
+      entry
+    end
+  end
+end
+
+def get_width(entries)
   entries.max_by(&:length).length + SPACE
 end
 
 def get_aligned_entries(row, entries)
-  # 項目の数が規定列数以下の場合、横に出力してしまうのでそれを回避する
-  if row < COLUMN
-    entries.each_slice(1).to_a
-  else
-    row_aligned_entries = entries.each_slice(row).to_a
-    row_aligned_entries.map! { |entries_col| entries_col.values_at(0...row) }.transpose
-  end
+  row_aligned_entries = entries.each_slice(row).to_a
+  row_aligned_entries.map { |col_entries| col_entries.values_at(0...row) }.transpose
 end
 
-def col_print(col, absolute_path, width)
+def print_col(col, width)
   col.each do |element|
     next if element.nil?
 
-    entry_path = absolute_path + element
-    if File.symlink?(entry_path)
-      print "#{element}@".ljust(width)
-    elsif File.directory?(entry_path)
-      print "#{element}/".ljust(width)
-    else
-      print element.ljust(width)
-    end
+    print element.ljust(width)
   end
 end
 
