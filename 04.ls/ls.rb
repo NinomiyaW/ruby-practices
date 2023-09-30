812970dfcf9fccc5b2fd7f04b14b4064fa17d97e
NinomiyaW <h.ninomiya@i3-systems.com>
2023-09-30 13:45:30 +0900

    ご指摘いただいた箇所を修正いたしました。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,26 +1,25 @@
 # frozen_string_literal: true
 
-COLUMN = 3
+COLUMN_COUNT = 3
 SPACE = 1
 
 def main
-  current_path = '../../dummy/' # "#{Dir.getwd}/"
-  current_entries = Dir.entries(current_path).sort
-  hidden_removed_entries = current_entries.delete_if { |entry| entry.start_with?('.') }
-  distinct_entries = distinguish_entries(hidden_removed_entries, current_path)
-  row = (distinct_entries.length.to_f / COLUMN).ceil
-  aligned_entries = align_entries(row, distinct_entries)
+  path =  "#{Dir.getwd}/"
+  entries = Dir.entries(path).sort
+  visible_entries = entries.delete_if { |entry| entry.start_with?('.') }
+  marked_entries = add_mark_end_of_entries(visible_entries, path)
+  row_count = (marked_entries.length.to_f / COLUMN_COUNT).ceil
+  aligned_entries = align_entries(row_count, marked_entries)
 
-  width = calculate_width(distinct_entries)
-  aligned_entries.each do |col_entries|
-    print_col(width, col_entries)
-    puts
+  width = calculate_width(marked_entries)
+  aligned_entries.each do |row_entries|
+    print_row_data(width, row_entries)
   end
 end
 
-def distinguish_entries(entries, absolute_path)
+def add_mark_end_of_entries(entries, path)
   entries.map do |entry|
-    entry_path = absolute_path + entry
+    entry_path = path + entry
     if File.symlink?(entry_path)
       "#{entry}@"
     elsif File.directory?(entry_path)
@@ -35,17 +34,16 @@ def calculate_width(entries)
   entries.max_by(&:length).length + SPACE
 end
 
-def align_entries(row, entries)
-  row_aligned_entries = entries.each_slice(row).to_a
-  row_aligned_entries.map { |col_entries| col_entries.values_at(0...row) }.transpose
+def align_entries(row_count, entries)
+  row_aligned_entries = entries.each_slice(row_count).to_a
+  row_aligned_entries.map { |col_entries| col_entries.values_at(0...row_count) }.transpose
 end
 
-def print_col(width, col_entries)
-  col_entries.each do |entry|
-    next if entry.nil?
-
-    print entry.ljust(width)
+def print_row_data(width, row_entries)
+  row_entries.each do |col_entry|
+    print col_entry.ljust(width) unless col_entry.nil?
   end
+  puts
 end
 
 main
